389b21e8423fe7f918b64cf2e4554dc3ce67bac9
Robert Kern <robert.kern@gmail.com>
2019-06-23 14:39:40 -0700

    ENH: increase stdin buffer size.
    http://www.pcg-random.org/downloads/practrand-0.93-bigbuffer.patch

--- tools/RNG_from_name.h
+++ tools/RNG_from_name.h
@@ -8,7 +8,7 @@ namespace Special_RNGs {
 			std::fprintf(stderr, "error reading standard input\n");
 			std::exit(0);
 		}
-		enum { BUFF_SIZE = 4096 / sizeof(Word) };
+		enum { BUFF_SIZE = 1048576 / sizeof(Word) };
 		Word *pos, *end;
 		bool ended;
 		Word buffer[BUFF_SIZE];
@@ -19,7 +19,10 @@ namespace Special_RNGs {
 			end = &buffer[n];
 		}
 	public:
-		_stdin_reader() : ended(false) { refill(); }
+		_stdin_reader() : ended(false) {
+		    setvbuf(stdin, 0, _IOFBF, 1048576);
+		    refill();
+		}
 		Word read() { Word rv = *(pos++); if (pos == end) refill(); return rv; }
 	};
 	class RNG_stdin : public PractRand::RNGs::vRNG8 {
